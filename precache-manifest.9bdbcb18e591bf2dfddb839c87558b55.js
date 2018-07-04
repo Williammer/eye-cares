93a260d0f8246e6d7272f7e5d110d9ec36a87a31
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-04 09:35:50 +0000

    Deploy Williammer/eye-cares to github.com/Williammer/eye-cares.git:gh-pages

---
+++ precache-manifest.9bdbcb18e591bf2dfddb839c87558b55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac6d336e029839b107d2",
+    "url": "./js/vendor.ac6d336e.js"
+  },
+  {
+    "revision": "27e3dd7809fdcb1943e5",
+    "url": "./js/manifest.27e3dd78.js"
+  },
+  {
+    "revision": "7e93f0ee21f87b680cc2",
+    "url": "./js/app.7e93f0ee.js"
+  },
+  {
+    "revision": "12d582235094838730e1b7c832627a6f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46bcae1f54a7e6f6a708d8aa703abe03",
+    "url": "./img/win.png"
+  },
+  {
+    "revision": "7e93f0ee21f87b680cc2",
+    "url": "./css/app.4a6e5fae.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.243f2df699f8efcef80bfcaed9e591cd.js"
+  "./precache-manifest.9bdbcb18e591bf2dfddb839c87558b55.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "eye-cares"});
