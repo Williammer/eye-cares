7614966bce517c4a9c52a20813857e591f31d494
William He <supervegeter@gmail.com>
2018-07-13 22:48:02 +0800

    Use Elena font

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,8 @@
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Elena, ElenaInitial;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
